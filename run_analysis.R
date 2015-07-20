6ec570c2d5249c6fce57b22b53c5a98dbec2e6be
008000 <008000@users.noreply.github.com>
2015-07-20 23:08:10 +0300

    crate read data function, merge data

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,23 @@
 features <- read.table('data/features.txt', col.names = c('number', 'feature'))
 activityLabels <- read.table('data/activity_labels.txt', col.names = c('code', 'activity'), colClasses = c('factor', 'factor'))
 
-# TODO: read all data (remove nrow limit)
-trainSubject <- read.table('data/train/subject_train.txt', nrows = 20, col.names = c('subject'), colClasses = c('factor'))
-testSubject <- read.table('data/test/subject_test.txt', nrows = 20, col.names = c('subject'), colClasses = c('factor'))
+readDataset <- function(mainDataFile, sibjectFile, activityFile) {
+    # TODO: read all data (remove nrow limit)
+    mainData <- read.table(mainDataFile, nrows = 40, col.names = features$feature)
+    subjectData <- read.table(sibjectFile, nrows = 40, col.names = c('subject'), colClasses = c('factor'))
+    activityData <- read.table(activityFile, nrows = 40, col.names = c('activityCode'), colClasses = c('factor'))
+    
+    selectedColumns <- grep("mean|std", names(mainData), ignore.case = TRUE)
+    mainData <- mainData[, selectedColumns]
+    
+    activityData = merge(activityData, activityLabels, by.x = "activityCode", by.y = "code", all.x = TRUE, sort = FALSE)
+    mainData$subject <- subjectData$subject
+    mainData$acivity <- activityData$activity
+    
+    mainData   
+}
 
-trainActivity <- read.table('data/train/y_train.txt', nrows = 20, col.names = c('activity'), colClasses = c('factor'))
-testActivity <- read.table('data/test/y_test.txt', nrows = 20, col.names = c('activity'), colClasses = c('factor'))
+trainData <- readDataset('data/train/X_train.txt', 'data/train/subject_train.txt', 'data/train/y_train.txt')
+testData <- readDataset('data/test/X_test.txt', 'data/test/subject_test.txt', 'data/test/y_test.txt')
 
-trainData <- read.table('data/train/X_train.txt', nrows = 20, col.names = features$feature)
-testData <- read.table('data/test/X_test.txt', nrows = 20, col.names = features$feature)
-
-#move to function
-selectedColumns <- grep("mean|std", names(trainData), ignore.case = TRUE)
-selctedData <- trainData[, selectedColumns]
+data <- rbind(trainData, testData)
