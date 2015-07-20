0eb62d6d66afa35b6770954f11e2453ad5eeaef3
008000 <008000@users.noreply.github.com>
2015-07-21 01:01:19 +0300

    preserve order when merging, create tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,35 @@
-features <- read.table('data/features.txt', col.names = c('number', 'feature'))
-activityLabels <- read.table('data/activity_labels.txt', col.names = c('code', 'activity'), colClasses = c('factor', 'factor'))
+library(reshape2)
+library(plyr)
+
+features <- read.table('data/features.txt', col.names=c('number', 'feature'))
+activityLabels <- read.table('data/activity_labels.txt', col.names=c('code', 'activity'), colClasses=c('factor', 'factor'))
+selectedColumnIndices <- grep("mean|std", features$feature, ignore.case=TRUE)
+featureColumnCount <- length(selectedColumnIndices)
 
 readDataset <- function(mainDataFile, sibjectFile, activityFile) {
-    # TODO: read all data (remove nrow limit)
-    mainData <- read.table(mainDataFile, nrows = 40, col.names = features$feature)
-    subjectData <- read.table(sibjectFile, nrows = 40, col.names = c('subject'), colClasses = c('factor'))
-    activityData <- read.table(activityFile, nrows = 40, col.names = c('activityCode'), colClasses = c('factor'))
+    subjectData <- read.table(sibjectFile, col.names=c('subject'), colClasses=c('factor'))
     
-    selectedColumns <- grep("mean|std", names(mainData), ignore.case = TRUE)
-    mainData <- mainData[, selectedColumns]
+    activityData <- read.table(activityFile, col.names=c('activityCode'), colClasses=c('factor'))
+    activityData$order <- 1:dim(activityData)[1]
+    activityData <- merge(activityData, activityLabels, by.x="activityCode", by.y="code", all.x=TRUE, sort=FALSE)
+    activityData <- arrange(activityData, order)
     
-    activityData = merge(activityData, activityLabels, by.x = "activityCode", by.y = "code", all.x = TRUE, sort = FALSE)
+    mainData <- read.table(mainDataFile, col.names=features$feature)
+    mainData <- mainData[, selectedColumnIndices]   
     mainData$subject <- subjectData$subject
-    mainData$acivity <- activityData$activity
+    mainData$activity <- activityData$activity
     
     mainData   
 }
 
 trainData <- readDataset('data/train/X_train.txt', 'data/train/subject_train.txt', 'data/train/y_train.txt')
 testData <- readDataset('data/test/X_test.txt', 'data/test/subject_test.txt', 'data/test/y_test.txt')
-
 data <- rbind(trainData, testData)
+
+tidyData <- melt(data, id=c("subject","activity"), measure.vars=names(data)[1:featureColumnCount])
+tidyData <- aggregate(value ~ subject + activity + variable, tidyData, mean)
+tidyData <- rename(tidyData, c("variable"="feature", "value"="averageValue"))
+tidyData <- arrange(tidyData, subject, activity, feature)
+
+write.table(tidyData, 'tidyData.txt', row.name=FALSE)
+
